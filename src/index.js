8f27cac36651ecc399deaed66203f49a2d21df6a
albertoouu <albertoouu@gmail.com>
2021-11-27 22:36:36 -0600

    add alumnos y botones

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ console.log(sede)
 const location = document.getElementById('sede')
 location.innerHTML = sede
 */
+/*------Temporal -----*/
+const location = document.getElementById('sede')
+let sede = "ajusco"
+location.innerHTML = sede
+/*------------------- */
 import { porcentajeCompletado, numeroEstudiantes, renderAlumnos, menosSesenta, masNoventa } from "./data.js";
