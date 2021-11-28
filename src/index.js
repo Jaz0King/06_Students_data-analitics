6907fd440b716e3795a5cefcd326b85bdc08c990
albertoouu <albertoouu@gmail.com>
2021-11-27 20:40:41 -0600

    change the background maps image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+/*
+const sede = localStorage['objectToPass']
+localStorage.removeItem('objectToPass')
+console.log(sede)
+const location = document.getElementById('sede')
+location.innerHTML = sede
+*/
 import { porcentajeCompletado, numeroEstudiantes, renderAlumnos, menosSesenta, masNoventa } from "./data.js";
-//renderAlumnos("ajusco","primera")
