ff74ffda666a3c8dea82d5de1f04b90e9bed932c
albertoouu <albertoouu@gmail.com>
2021-11-29 23:59:34 -0600

    add porcentaje y alumnos 60 90

--- src/data.js
+++ src/data.js
@@ -1,69 +1,59 @@
-export function numeroEstudiantes(sede,gen) {
-//console.log(sede)
+export function estudiantes(sede, gen) {
+  //console.log(sede)
   fetch("../data/students.json")
     .then((response) => response.json())
-    .then((data) => studentsNumber(sede, gen, data))
+    .then((data) => {
+      studentsNumber(sede, gen, data)
+      porcentage(sede, gen, data)
+      render(sede, gen, data)
+      alumnosMenosSesenta(sede, gen, data)
+      alumnosMasNoventa(sede, gen, data)
+    })
     .catch((error) => console.log(error));
 }
-function studentsNumber (sede, gen, data) {
+function studentsNumber(sede, gen, data) {
   console.log(data)
   console.log(data[sede].generacion[gen].estudiantes.length)
+  let num = document.getElementById('numeroAlumnos')
+  num.innerHTML += data[sede].generacion[gen].estudiantes.length
 }
 
-export function porcentajeCompletado(sede,gen) {
-  fetch("../data/students.json")
-    .then((response) => response.json())
-    .then((data) => porcentage(sede, gen, data))
-    .catch((error) => console.log(error));
-}
-function porcentage (sede, gen, data) {
-      let x = 0;
-      for (let i =0 ; i<data[sede].generacion[gen].estudiantes.length; i++) {
-        x += data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado
-      }
-      x = x/(data[sede].generacion[gen].estudiantes.length)
-      console.log(x)
+function porcentage(sede, gen, data) {
+  let x = 0;
+  for (let i = 0; i < data[sede].generacion[gen].estudiantes.length; i++) {
+    x += data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado
+  }
+  x = x / (data[sede].generacion[gen].estudiantes.length)
+  console.log(x)
+  let porc = document.getElementById('porcentaje')
+  porc.innerHTML += x
 }
 
-export function renderAlumnos (sede,gen) {
-  fetch("../data/students.json")
-    .then((response) => response.json())
-    .then((data) => render(sede, gen, data))
-    .catch((error) => console.log(error))
-}
-function render (sede, gen, data) {
+function render(sede, gen, data) {
   console.log(data)
-  for (let i=0; i<data[sede].generacion[gen].estudiantes.length; i++) {
+  for (let i = 0; i < data[sede].generacion[gen].estudiantes.length; i++) {
     console.log(data[sede].generacion[gen].estudiantes[i].nombre)
   }
 }
 
-export function menosSesenta (sede,gen) {
-    fetch("../data/students.json")
-    .then((response) => response.json())
-    .then((data) => alumnosMenosSesenta(sede, gen, data))
-    .catch((error) => console.log(error))
-}
-function alumnosMenosSesenta (sede, gen, data) {
-  for (let i = 0; i<data[sede].generacion[gen].estudiantes.length; i++) {
+function alumnosMenosSesenta(sede, gen, data) {
+  let menos60 = document.getElementById('menos60')
+  for (let i = 0; i < data[sede].generacion[gen].estudiantes.length; i++) {
     //console.log(data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado)
-    if(data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado < 60) {
-      console.log(data[sede].generacion[gen].estudiantes[i].nombre)
-    } else {}
+    if (data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado < 60) {
+      console.log('menos de sesenta: ' + data[sede].generacion[gen].estudiantes[i].nombre)
+      menos60.innerHTML += data[sede].generacion[gen].estudiantes[i].nombre + `<br>`
+    } else { }
   }
 }
 
-export function masNoventa (sede,gen) {
-    fetch("../data/students.json")
-    .then((response) => response.json())
-    .then((data) => alumnosMasNoventa(sede, gen, data))
-    .catch((error) => console.log(error))
-}
-function alumnosMasNoventa (sede, gen, data) {
-  for (let i = 0; i<data[sede].generacion[gen].estudiantes.length; i++) {
+function alumnosMasNoventa(sede, gen, data) {
+  let mas90 = document.getElementById('mas90')
+  for (let i = 0; i < data[sede].generacion[gen].estudiantes.length; i++) {
     //console.log(data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado)
-    if(data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado > 90) {
-      console.log(data[sede].generacion[gen].estudiantes[i].nombre)
-    } else {}
+    if (data[sede].generacion[gen].estudiantes[i].progreso.porcentajeCompletado > 90) {
+      console.log('mas de noventa: ' + data[sede].generacion[gen].estudiantes[i].nombre)
+      mas90.innerHTML += data[sede].generacion[gen].estudiantes[i].nombre + `<br>`
+    } else { }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+/*
 const sede = localStorage['objectToPass']
 localStorage.removeItem('objectToPass')
 console.log(sede)
 const location = document.getElementById('sede')
 location.innerHTML = sede
-if(sede=='ajusco'){
+*/
+let sede = 'ajusco'
+let gen = 'tercera'
+if (sede == 'ajusco') {
   let mapa = document.getElementById('marcoImagen')
-  mapa.innerHTML =`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6015.2682977899585!2d-99.1975469513815!3d19.225598784554762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdfdf6a3ef005d%3A0xb4c2a9c688e66926!2zMTnCsDEzJzI4LjQiTiA5OcKwMTEnMzkuNyJX!5e0!3m2!1ses!2smx!4v1638135188085!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
-}else {
-  if(sede=='chapultepec'){
+  mapa.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6015.2682977899585!2d-99.1975469513815!3d19.225598784554762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdfdf6a3ef005d%3A0xb4c2a9c688e66926!2zMTnCsDEzJzI4LjQiTiA5OcKwMTEnMzkuNyJX!5e0!3m2!1ses!2smx!4v1638135188085!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+} else {
+  if (sede == 'chapultepec') {
 
-  }else {
+  } else {
 
   }
 }
@@ -38,5 +42,5 @@ location.innerHTML = sede
 /*------------------- */
 
 
-import { porcentajeCompletado, numeroEstudiantes, renderAlumnos, menosSesenta, masNoventa } from "./data.js";
-numeroEstudiantes(sede,"primera")
+import { estudiantes } from "./data.js";
+estudiantes(sede, gen)
