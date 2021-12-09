4693d58d8b80f320a48349f78a2ddccab094ac2f
Jazito <jazo@Jazito-Air.local>
2021-12-08 19:30:33 -0600

    login incorrecto

--- src/login.js
+++ src/login.js
@@ -3,10 +3,18 @@ let entrar = document.getElementById('botonEntrar')
 entrar.addEventListener('click', () =>{
     usuario= document.getElementById('inputP1').value
     contraseña= document.getElementById('inputP2').value
+    video = document.getElementById('login')
     console.log(usuario,contraseña)
-    if(usuario=="director" || contraseña=="123") {
+    if(usuario=="directora" && contraseña=="123") {
         window.location.href = "./sucursales.html"
     } else {
-        alert("Usuario y/o contraseña incorrectos")
+        //alert("Usuario y/o contraseña incorrectos")
+        
+        video.innerHTML = `
+            <img id="tronchaToro" src="../assets/imágenes/directora.jpeg">
+            <audio autoplay><source src="../assets/audio/palabraMágica.ogg" type="video/mp4"></audio>
+            `
+        console.log(error)
+
     }
 })
